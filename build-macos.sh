170172202846d21ccc9aed036708d3ce7f6378f5
Andrea Medeghini <andrea@nextbreakpoint.com>
2016-07-30 17:29:38 +0100

    Restored checkout from git repo

--- build-macos.sh
+++ build-macos.sh
@@ -1,17 +1,13 @@
 #/bin/sh
 export BUILD_DIR=`pwd`/build
 
-#rm -fR ffmpeg
+rm -fR ffmpeg
 
-#git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg
+git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg
 
 cp Makefile.lib.macos ffmpeg/Makefile.lib.macos
 
 cp Makefile.jni.macos ffmpeg/Makefile.jni.macos
-
-#cp macos.patch ffmpeg/macos.patch
-
-#patch -p0 < ffmpeg/macos.patch
 
 cd ffmpeg
 
