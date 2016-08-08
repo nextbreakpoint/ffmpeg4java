4817b0d4da2e921e1dda8086136b56806076f7fa
Andrea Medeghini <andrea@nextbreakpoint.com>
2016-08-08 09:38:26 +0100

    Fixed regression

--- build-macos.sh
+++ build-macos.sh
@@ -8,13 +8,14 @@ git clone https://github.com/nextbreakpoint/FFmpeg.git ffmpeg
 cp Makefile.lib.macos ffmpeg/Makefile.lib.macos
 cp Makefile.jni.macos ffmpeg/Makefile.jni.macos
 
+cp Patch.macos ffmpeg/Patch.macos
+
 cd ffmpeg
 
 git checkout ce36e74e75751c721185fbebaa4ee8714b44c5a5
 
 cd ..
 
-cp Patch.macos ffmpeg/Patch.macos
 patch -p0 < ffmpeg/Patch.macos
 
 cd ffmpeg
--- build-mingw64.sh
+++ build-mingw64.sh
@@ -7,7 +7,6 @@ git checkout ce36e74e75751c721185fbebaa4ee8714b44c5a5
 
 cd ..
 
-Patch.mingw64 ffmpeg/Patch.mingw64
 patch -p0 < ffmpeg/Patch.mingw64
 
 cd ffmpeg
