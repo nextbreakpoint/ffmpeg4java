9c07783069682521568b2edb948d43333678fee6
Andrea Medeghini <andrea@nextbreakpoint.com>
2016-08-08 07:33:23 +0100

    Checkout specific version of ffmpeg

--- build-linux.sh
+++ build-linux.sh
@@ -3,6 +3,15 @@ export BUILD_DIR=`pwd`/build
 
 cd ffmpeg
 
+git checkout ce36e74e75751c721185fbebaa4ee8714b44c5a5
+
+cd ..
+
+Patch.linux ffmpeg/Patch.linux
+patch -p0 < ffmpeg/Patch.linux
+
+cd ffmpeg
+
 make -f Makefile.lib.linux ffmpeg
 
 make -f Makefile.jni.linux all
--- build-macos.sh
+++ build-macos.sh
@@ -8,6 +8,12 @@ git clone https://github.com/nextbreakpoint/FFmpeg.git ffmpeg
 cp Makefile.lib.macos ffmpeg/Makefile.lib.macos
 cp Makefile.jni.macos ffmpeg/Makefile.jni.macos
 
+cd ffmpeg
+
+git checkout ce36e74e75751c721185fbebaa4ee8714b44c5a5
+
+cd ..
+
 cp Patch.macos ffmpeg/Patch.macos
 patch -p0 < ffmpeg/Patch.macos
 
